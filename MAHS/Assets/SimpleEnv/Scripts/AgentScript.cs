48c9afbc67e14b2b32c0dc52e63bbadb7c5f8128
Тарасов Денис <dt6cgsg@gmail.com>
2021-03-17 22:58:30 +0300

    Create simple env

---
+++ MAHS/Assets/SimpleEnv/Scripts/AgentScript.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Policies;
+using Debug = UnityEngine.Debug;
+
+public class AgentScript : Agent
+{
+    public enum Team
+    {
+        Hider = 0,
+        Seeker = 1
+    }
+
+    private const int NUM_TEAMS = (int) Team.Seeker;
+    
+    public TrainingAreaScript area;
+    private Rigidbody rBody;
+    private ViewField vf;
+    BehaviorParameters behaviorParameters;
+    [HideInInspector]
+    public Team team;
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        vf = GetComponent<ViewField>();
+        behaviorParameters = GetComponent<BehaviorParameters>();
+        
+        if (behaviorParameters.TeamId == (int) Team.Hider)
+        {
+            team = Team.Hider;
+        }
+        else
+        {
+            team = Team.Seeker; 
+        }
+
+        this.MaxStep = area.episodeLength;
+        
+        area.agents.Add(this);
+    }
+    
+    public override void OnEpisodeBegin()
+    {
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+        this.transform.Rotate(0,Random.value * 360 - 180f, 0);
+        
+        if (team == Team.Hider)
+        {
+            this.transform.localPosition = new Vector3(Random.value * 4.25f + 0.25f, 0.5f, -Random.value * 4.25f - 0.25f);
+        }
+        else
+        {
+            this.transform.localPosition = new Vector3(-Random.value * 5f + 0.5f, 0.5f, Random.value * 9f - 4.5f);
+        }
+        
+        area.ResetEnv();
+    }
+    
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        //Debug.Log(this.GetCumulativeReward());
+        // Agent role
+        sensor.AddOneHotObservation((int)team, NUM_TEAMS);
+        
+        // Agent position
+        sensor.AddObservation(transform.localPosition.x);
+        sensor.AddObservation(transform.localPosition.z);
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+
+        Quaternion opponentInfo = new Quaternion(0, 0, 0, 0);
+        foreach (GameObject obj in vf.collectVisibleObjects())
+        {
+            if (obj.CompareTag("Hider") || obj.CompareTag("Seeker"))
+            {
+                if (team == Team.Seeker)
+                {
+                    area.isAnyHiderSeen = true;
+                }
+
+                Rigidbody otherAgentRigidbody = obj.GetComponent<Rigidbody>();
+                // Opponent position
+                opponentInfo.x =  obj.transform.localPosition.x;
+                opponentInfo.y =  obj.transform.localPosition.z;
+                // Opponent velocity
+                opponentInfo.z = otherAgentRigidbody.velocity.x;
+                opponentInfo.w = otherAgentRigidbody.velocity.z;
+                break;
+            }
+        }
+        
+        sensor.AddObservation(opponentInfo.x);
+        sensor.AddObservation(opponentInfo.y);
+        sensor.AddObservation(opponentInfo.z);
+        sensor.AddObservation(opponentInfo.w);
+    }
+    
+    public float forceMultiplier = 10.0f;
+    public float rotationMultiplier = 1.0f;
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        //Debug.Log(this.StepCount);
+        if (team == Team.Seeker && this.StepCount <= area.preparingPhaseLength)
+            return;
+        
+        Vector3 controlSignal = Vector3.zero;
+        
+        controlSignal.x = actions.ContinuousActions[0];
+        controlSignal.z = actions.ContinuousActions[1];
+        //Debug.Log(controlSignal);
+        rBody.AddForce(controlSignal * forceMultiplier);
+        rBody.AddTorque(transform.up * rotationMultiplier * actions.ContinuousActions[2]);
+    }
+    
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        //Debug.Log("Heuristic called");
+        var continiousActionsOut = actionsOut.ContinuousActions;
+        continiousActionsOut[0] = Input.GetAxis("Horizontal");
+        continiousActionsOut[1] = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.Q))
+            continiousActionsOut[2] = -1.0f;
+        else if (Input.GetKey(KeyCode.E))
+            continiousActionsOut[2] = 1.0f;
+        else
+            continiousActionsOut[2] = 0.0f;
+    }
+}
---
+++ MAHS/Assets/SimpleEnv/Scripts/TrainingAreaScript.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Unity.MLAgents;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TrainingAreaScript : MonoBehaviour
+{
+    [HideInInspector]
+    public bool isAnyHiderSeen;
+    [HideInInspector]
+    public List<AgentScript> agents;
+    void Awake()
+    {
+        Academy.Instance.OnEnvironmentReset += ResetEnv;
+        Academy.Instance.AgentPreStep += PreStep;
+    }
+
+    public int episodeLength = 1000;
+    public int preparingPhaseLength = 400;
+    
+    public Transform box1;
+    public Transform box2;
+    public void ResetEnv()
+    {
+        box1.transform.localPosition = new Vector3(Random.value * 4.25f + 0.25f, 0.5f, -Random.value * 4.25f - 0.25f);
+        box2.transform.localPosition = new Vector3(Random.value * 4.25f + 0.25f, 0.5f, -Random.value * 4.25f - 0.25f);
+    }
+
+    void PreStep(int i)
+    {
+        float hidersReward = 1.0f;
+        if (isAnyHiderSeen)
+            hidersReward = -1.0f;
+        
+        foreach (AgentScript agent in agents)
+        {
+            if (agent.StepCount < preparingPhaseLength)
+                break;
+            if (agent.team == AgentScript.Team.Hider)
+                agent.AddReward(hidersReward);
+            else
+                agent.AddReward(-hidersReward);
+        }
+        //Debug.Log("IsSeen:" + isAnyHiderSeen + i);
+        isAnyHiderSeen = false;
+    }
+
+}
